5d0314dc431092941a35c6886ffbf20f0b6ae0c0
Kim <kimyongmin@robot.t.u-tokyo.ac.jp>
2024-01-02 12:07:37 +0900

    chore: add gpt3 config.
    add gpt3 config to llm_config.

--- config/__init__.py
+++ config/__init__.py
@@ -1,1 +1,1 @@
-from .llm_config import gpt4_config, gpt4v_config
+from .llm_config import gpt4_config, gpt4v_config, gpt3_config
--- config/llm_config.py
+++ config/llm_config.py
@@ -19,5 +19,13 @@ config_list_gpt4 = autogen.config_list_from_json(
     },
 )
 
+config_list_gpt3 = autogen.config_list_from_json(
+    oai_config_path,
+    filter_dict={
+        "model": ["gpt-3.5-turbo-1106"],
+    },
+)
+
 gpt4_config = {"config_list": config_list_gpt4, "max_tokens": 2000, "cache_seed": 42}
 gpt4v_config = {"config_list": config_list_4v, "max_tokens": 2000, "cache_seed": 42}
+gpt3_config = {"config_list": config_list_gpt3, "max_tokens": 500, "cache_seed": 42}
